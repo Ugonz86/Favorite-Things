29ce38389e22f3eaba62dc802b1668e12b189ee0
Sharon Lee and Uriel Gonzalez <sharon@>
2019-10-28 11:47:34 -0700

    Add comments to explaint variables.

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,15 @@ $(document).ready(function() {
       var food = $("input#foodForm").val();
       var places = $("input#placesForm").val();
       var color = $("input#colorForm").val();
-      var result1 = [food, places, color]
-      var result2 = result1[1]
-      var result3 = result1[0]
-      var result4 = result1[2]
-      var result5 = result1.slice(1);
-      var result6 = result5.slice(1);
+      var result1 = [name, food, places, color] //This complilation of all the variables into one array
+      var result2 = result1[1] //This will access a specific element.
+      var result3 = result1[0] //This will access a specific element.
+      var result4 = result1[2] //This will access a specific element.
+      var result5 = result1.slice(0, 3); //This will slice the array from the first element until the last, which in this case it leaves the 3rd element out which is var color.
+      console.log(result5 + " copy of array"); //
+      var result6 = result5.slice(1); //This will cut the array starting at the 1st element.
       var newArray = [];
-      newArray.push(result6, food, places);
+      newArray.push(result6, food, places); //This is an empty new array where we push or add the last array with the others.
 
 
       $("#name").text(name);
